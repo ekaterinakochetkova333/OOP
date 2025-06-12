4524b0caeb65cd81334485612b0268ccbf9b8674
kochetkova <salomatova2021@inbox.ru>
2025-06-12 23:13:02 +0700

    new func

--- src/category.py
+++ src/category.py
@@ -8,9 +8,30 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, list_products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = list_products
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(list_products) if list_products else 0
+
+
+    def add_product(self, products):
+        self.__products.append(products)
+
+
+    @property
+    def products(self):
+        desc_products = []
+        for products in self.__products:
+            desc_products.append(f'{products.name}, {products.price} руб. Остаток: {products.quantity} шт.')
+        return desc_products
+
+    @products.setter
+    def products(self, products: Product):
+        self.__products.append(products)
+        Category.product_count += 1
+
+
+
+# Название продукта, 80 руб. Остаток: 15 шт.
--- src/product.py
+++ src/product.py
@@ -7,5 +7,36 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product):
+        name = product["name"]
+        description = product["description"]
+        price = product["price"]
+        quantity = product["quantity"]
+        return Product(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if float(price) <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        else:
+            self.__price = price
+            return
+
+
+
+
+
+
+
+
+
+
